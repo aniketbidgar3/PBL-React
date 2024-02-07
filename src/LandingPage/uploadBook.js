cf4444f3a5d0b8a46695140072cbb0f988f35253
aniketbidgar3 <aniketbidgar3@gmail.com>
2024-02-08 00:08:27 +0530

    "Change in About us page"

--- src/LandingPage/uploadBook.js
+++ src/LandingPage/uploadBook.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function UploadBook() {
- 
-
   return (
     <div className="text-center">
-        <h4>we are a second hand book store founded in 2023 currently in developing mode . one day we will change life of poor students who cant afford to buy books,we provide them second hand books at low price our mission is to provide books to all students wh want to change teir lives ,and our slogan is that "books can be second hand but knowledge cant".thank you for visiting our website</h4>
+      <h4>
+        Calling all students! Dive into the world of literature and make a
+        difference with Bookshelf2nd. Your love for reading can ignite a spark
+        of change by donating books to our platform. Imagine the joy of passing
+        on your favorite stories to fellow bookworms, sharing adventures,
+        knowledge, and inspiration with others. By contributing to Bookshelf2nd,
+        you're not just decluttering your shelves; you're opening doors to new
+        worlds for readers everywhere. Let's spread the magic of storytelling
+        and build a community where every book finds a new home and every reader
+        discovers their next adventure. Join us at Bookshelf2nd and be a part of
+        something truly special!
+      </h4>
     </div>
   );
 }
