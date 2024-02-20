6663e7bd537a95e7f261d8da922282c97d6aefc8
aniketbidgar3 <aniketbidgar3@gmail.com>
2024-02-20 16:33:40 +0530

    "Just Deployed My Project"

--- src/LandingPage/BookList.js
+++ src/LandingPage/BookList.js
@@ -1,104 +1,104 @@
-import BookCard from "./Component/BookCard";
+// import BookCard from "./Component/BookCard";
 
-const booksDetail=[
+// const booksDetail=[
    
     
-    {
-        bookName:"All FE Books",
-        bookPrice:2000,
-        bookPubliser:"Techneo ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All FE Books",
-        bookPrice:2000,
-        bookPubliser:"Techknowledge ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All FE Books",
-        bookPrice:2000,
-        bookPubliser:"Nirali ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All SE Books",
-        bookPrice:2000,
-        bookPubliser:"Techneo ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All SE Books",
-        bookPrice:2000,
-        bookPubliser:"Techknowledge ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All SE Books",
-        bookPrice:2000,
-        bookPubliser:"Nirali ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All TE Books",
-        bookPrice:2000,
-        bookPubliser:"Techneo ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All TE Books",
-        bookPrice:2000,
-        bookPubliser:"Techknowledge ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All TE Books",
-        bookPrice:2000,
-        bookPubliser:"Nirali ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All BE Books",
-        bookPrice:2000,
-        bookPubliser:"Techneo ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All BE Books",
-        bookPrice:2000,
-        bookPubliser:"Techknowledge ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    },
-    {
-        bookName:"All BE Books",
-        bookPrice:2000,
-        bookPubliser:"Nirali ",
-        bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
-        ,button:"Contact Owner"
-    }
-]
+//     {
+//         bookName:"All FE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techneo ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All FE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techknowledge ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All FE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Nirali ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All SE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techneo ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All SE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techknowledge ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All SE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Nirali ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All TE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techneo ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All TE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techknowledge ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All TE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Nirali ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All BE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techneo ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All BE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Techknowledge ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     },
+//     {
+//         bookName:"All BE Books",
+//         bookPrice:2000,
+//         bookPubliser:"Nirali ",
+//         bookCity:"Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer ,posuere erat a ante"
+//         ,button:"Contact Owner"
+//     }
+// ]
 
-function BookList() {
-    return (
-        <div className="row">
+// function BookList() {
+//     return (
+//         <div className="row">
             
-            {
-                booksDetail.map((data)=><BookCard bookData={data}/>)
-            }
+//             {
+//                 booksDetail.map((data)=><BookCard bookData={data}/>)
+//             }
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
 
-export default BookList;
+// export default BookList;
